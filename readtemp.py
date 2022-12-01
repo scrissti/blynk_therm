8143174a3dbb9cd33c1647a8e3ea2ef24572a0fd
Cristian Serban <>
2022-12-01 20:23:11 +0200

    BlynkLib include

--- readtemp.py
+++ readtemp.py
@@ -17,7 +17,7 @@ blynk=0
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(18, GPIO.OUT)
 GPIO.output(18, GPIO.LOW)
-print "off"
+print("off")
 min_temp=43
 max_temp=63
 stat_rel=0
@@ -26,7 +26,7 @@ try:
     BLYNK_AUTH = ''
     blynk = BlynkLib.Blynk(BLYNK_AUTH)
 except:
-    print "no internet for blynk"
+    print("no internet for blynk")
 
 def read_temp_raw():
     f = open(device_file, 'r')
@@ -49,34 +49,34 @@ def read_temp():
 def v3_write_handler(value):
     global min_temp
     min_temp=float(value)
-    print "new min is " + value
+    print("new min is ",value)
 
 @blynk.VIRTUAL_WRITE(4)
 def v4_write_handler(value):
     global max_temp
     max_temp=float(value)
-    print "new max is " + value
+    print("new max is ",value)
 
 @blynk.VIRTUAL_READ(2)
 def v2_read_handler():
     y = float(read_temp()) #(np.cos(k*i/50.)*np.cos(i/50.)+np.random.randn(1))[0]
-    print y
-    print min_temp
-    print max_temp
+    print(y)
+    print(min_temp)
+    print(max_temp)
 
     if y>max_temp:
-        print "off"
+        print("off")
         GPIO.output(18, GPIO.LOW)
         stat_rel = 0
     if y<min_temp:
-        print "on"
+        print("on")
         GPIO.output(18, GPIO.HIGH)
         stat_rel = 1
     try:
         blynk.virtual_write(2,y)
         blynk.virtual_write(7,stat_rel)
     except:
-        print sys.exc_info()[0]
+        print(sys.exc_info()[0])
 
 def handler(signum, frame):
     v2_read_handler()
